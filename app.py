1875bcba9c36c8a66659a5fe64c54bfc71618c5f
Trinity Sazo <tsazo@andover.edu>
2020-05-07 14:18:30 -0400

    index.html, about.html, and style updates
    Successfully added CSS to the templates. Mostly completed the index pages with all the required information (links and all). Started formatting about.html with information of my choice.

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 21 14:57:17 2020
 
-@author: etill
-"""
 
 #import statements
 from flask import Flask, render_template
@@ -13,15 +8,6 @@ app = Flask(__name__)
 
 #static route
 @app.route('/')
-def style():
-    style = """"""
-
-def name():
-    return "<div style='text-align:center;'>Hi my name is Trinity Sazo. Trin for short.</div>"
-
-def picture():
-    return ""
-
 def index():
     return render_template('index.html')
 
@@ -30,7 +16,7 @@ def about():
     return render_template('about.html')
 
 @app.route('/columbia')
-def about():
+def columbia():
     return render_template('columbia.html')
 
 #start the server
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Trinity Sazo
+ts3185
+Final 
+
+Links and most of the body of the code is in the template files. I used Flask primarily to run the files and use the function of the templates to link the different webpages together. 
+
+Image Citations:
